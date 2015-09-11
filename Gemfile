2a27464cab145e6fcf0204e9edb2aee52bbbdd6a
aristeuroriz <aristeuroriz@gmail.com>
2015-09-11 15:04:13 -0300

    Omniauth Implemented

--- Gemfile
+++ Gemfile
@@ -14,7 +14,10 @@ gem 'jbuilder', '~> 2.0'
 gem 'sdoc', '~> 0.4.0', group: :doc
 
 gem 'devise'
+gem 'omniauth-twitter'
+gem 'omniauth-facebook'
 
+gem 'dotenv-rails'
 
 group :development, :test do
 	gem 'mysql2', '~> 0.3.20'
---
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -0,0 +1,37 @@
+class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
+
+ def facebook
+    # You need to implement the method below in your model (e.g. app/models/user.rb)
+    @user = User.find_for_facebook_oauth(request.env["omniauth.auth"])
+
+    if @user.persisted?
+
+      sign_in(@user)
+
+      if @user.email.blank?
+        redirect_to edit_user_registration_path, notice: 'Agora é necessário e muito importante completar seu cadastro!'
+      else
+        redirect_to root_path
+        set_flash_message(:notice, :success, :kind => "facebook") if is_navigational_format?
+      end
+
+    else
+      session["devise.facebook_data"] = request.env["omniauth.auth"]
+      redirect_to new_user_registration_url
+    end
+  end
+
+   def twitter
+      @user = User.from_omniauth(request.env["omniauth.auth"])
+      logger.info( "==> ominiauth.twitter.user = #{@user.inspect}" )
+
+      if @user
+        sign_in_and_redirect @user, event: :authentication, notice: "Signed in!"
+        set_flash_message(:notice, :success, :kind => "Twitter") if is_navigational_format?
+      else
+        session["devise.twitter_data"] = request.env["omniauth.auth"]
+        redirect_to new_user_registration_url
+      end
+   end
+
+end
